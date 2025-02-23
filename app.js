fdf67da66b4236bab1df0dba2c8ca7da75cc4878
Kiany <sandovalkiany@gmail.com>
2025-02-22 22:56:37 -0600

    Actualizo el archivo app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundoooo!');
+  res.send('¡Continuous Deployment funcionando!');
 });
 
 app.listen(port, () => {
